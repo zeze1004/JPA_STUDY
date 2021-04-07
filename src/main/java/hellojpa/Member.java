3b99a0e5fa0bdd73ddae00caba8c93b9fc0de68b
zeze1004 <thwjd2717@naver.com>
2021-04-07 12:32:14 +0900

    add ch03 note

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -8,6 +10,7 @@ import java.util.Date;
 @Entity(name = "Member") // 디폴트로 클래스 이름을 그대로 사용
 public class Member {
     @Id
+    @GeneratedValue(strategy = )
     private Long id;
 
     // 컬럼 매핑
